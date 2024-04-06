ff3b208e3bdc033c41e51c09ea5f8abb4dfa740b
julius <nyerereoffice@gmail.com>
2024-04-06 15:51:54 +0300

    Cart View

--- nordichomes/nordichomes/urls.py
+++ nordichomes/nordichomes/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('core.urls')),
     path('', include('product.urls')),
+    path('', include('cart.urls')),
     path('admin/', admin.site.urls),
 ]
