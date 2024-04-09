c290080c8ed67afee8158f3d9fce80ed4143f798
julius <nyerereoffice@gmail.com>
2024-04-09 04:44:57 +0300

    Log in Page

--- nordichomes/core/urls.py
+++ nordichomes/core/urls.py
@@ -10,6 +10,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.frontpage, name='frontpage'),
-    path('signup', views.signup, name='signup'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
     path('shop/', views.shop, name='shop'),
 ]
--- nordichomes/core/views.py
+++ nordichomes/core/views.py
@@ -14,8 +14,15 @@ def frontpage(request):
 
 
 
+def login(request):
+    return render(request, 'core/login.html')
+
+
+
+
 def signup(request):
     return render(request, 'core/signup.html')
+
 
 
 
