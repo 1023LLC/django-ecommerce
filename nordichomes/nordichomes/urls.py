80d4ccebd707991ccb3e6519ec77f9aaaf91f986
julius <nyerereoffice@gmail.com>
2024-04-11 06:41:08 +0300

    Urls Fix

--- nordichomes/nordichomes/urls.py
+++ nordichomes/nordichomes/urls.py
@@ -12,6 +12,6 @@ from django.contrib.auth import views
 urlpatterns = [
     path('', include('core.urls')),
     path('', include('product.urls')),
-    path('', include('cart.urls')),
+    path('cart/', include('cart.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
