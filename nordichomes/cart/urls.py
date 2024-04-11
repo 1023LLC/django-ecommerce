c23ad632221e713c133689c58ee28b29fd9d6561
julius <nyerereoffice@gmail.com>
2024-04-11 05:16:12 +0300

    Cart Page

--- nordichomes/cart/urls.py
+++ nordichomes/cart/urls.py
@@ -6,5 +6,6 @@ from . import views
 app_name = 'cart'
 
 urlpatterns = [
-    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart')
+    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.cart, name='cart'),
 ]
