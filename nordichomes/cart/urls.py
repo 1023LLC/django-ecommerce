5f322a87a97b63c5af51b3a562e0346d452123b3
julius <nyerereoffice@gmail.com>
2024-04-14 13:58:31 +0300

    Add HTMX Attributes to Cart Buttons

--- nordichomes/cart/urls.py
+++ nordichomes/cart/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('update_cart/<int:product_id>/<str:action>/', views.update_cart, name='update_cart'),
     path('hx_menu_cart/', views.hx_menu_cart, name="hx_menu_cart"),
 ]
