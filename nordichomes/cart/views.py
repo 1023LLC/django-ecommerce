7b419504232634005647739a2c5338bfeaf13e5b
julius <nyerereoffice@gmail.com>
2024-04-14 12:38:31 +0300

    Update Cart View & Routing

--- nordichomes/cart/views.py
+++ nordichomes/cart/views.py
@@ -4,6 +4,8 @@ from .cart import Cart
 
 from django.contrib.auth.decorators import login_required
 
+from product.models import Product
+
 
 def add_to_cart(request, product_id):
     cart = Cart(request)
@@ -16,12 +18,42 @@ def cart(request):
     return render(request, 'cart/cart.html')
 
 
+
+def update_cart(request, product_id, action):
+    cart = Cart(request)
+    
+    if action == 'increment':
+        cart.add(product_id, 1, True)
+    else:
+        cart.add(product_id, -1, True)
+        
+    product = Product.objects.get(pk=product_id)
+    quantity = cart.get_item(product_id)
+    
+    item = {
+        'product': {
+            'id': product_id,
+            'name': product.name,
+            'image': product.image,
+            'get_thumbnail': product.get_thumbnail(),
+            'price': product.price,
+        },
+        'total_price': (quantity * product.price) / 100,
+        'quantity': quantity,
+    }
+    
+    response = render(request, 'cart/partials/cart_item.html', {'item': item})
+    response['HX-Trigger'] = 'update-menu-cart'
+    
+    return response
+
+
+
 @login_required
 def checkout(request):
     return render(request, 'cart/checkout.html')
 
 
 
-@login_required
 def hx_menu_cart(request):
     return render(request, 'cart/menu_cart.html')
