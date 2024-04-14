8b96aea505b82257b08a52fe0685f6b9eb6236ee
julius <nyerereoffice@gmail.com>
2024-04-14 15:11:29 +0300

    Update Total Price

--- nordichomes/cart/urls.py
+++ nordichomes/cart/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('update_cart/<int:product_id>/<str:action>/', views.update_cart, name='update_cart'),
     path('hx_menu_cart/', views.hx_menu_cart, name="hx_menu_cart"),
+    path('hx_cart_total/', views.hx_cart_total, name="hx_cart_total"),
 ]
