47cad94112a6c23b0e7ef0d01b928713e4fc75da
julius <nyerereoffice@gmail.com>
2024-04-16 17:21:02 +0300

    Url Routing

--- nordichomes/nordichomes/urls.py
+++ nordichomes/nordichomes/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path('', include('product.urls')),
     path('cart/', include('cart.urls')),
+    path('order/', include('order.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
