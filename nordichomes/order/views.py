8330d0afefdb25d186b2d03265cabb2ca991b0f6
julius <nyerereoffice@gmail.com>
2024-04-17 13:59:20 +0300

    Checkout Session View w/Stripe

--- nordichomes/order/views.py
+++ nordichomes/order/views.py
@@ -1,33 +1,75 @@
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
 
 from .models import Order, OrderItem
 
 from cart.cart import Cart
 
+import json
+import stripe
+from django.conf import settings
+
+
 
 def start_order(request):
     
     cart = Cart(request)
     
-    if request.method == 'POST':
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        email = request.POST.get('email')
-        address = request.POST.get('address')
-        zipcode = request.POST.get('zipcode')
-        place = request.POST.get('place')
-        phone = request.POST.get('phone')
-        
-        order = Order.objects.create(user=request.user, first_name=first_name, last_name=last_name, email=email, phone=phone, address=address, zipcode=zipcode, place=place)
-        
-        
-        for item in cart:
-            product = item['product']
-            quantity = int(item['quantity'])
-            price = product.price * quantity
-            
-            item = OrderItem.objects.create(order=order, product=product, price=price, quantity=quantity)
-            
-        return redirect('core:myaccount')
+    data = json.loads(request.body)
     
-    return redirect('cart:cart')
+    total_price = 0
+    
+    items = []
+    
+    for item in cart:
+        product = item['product']
+        total_price += product.price * int(item['quantity'])
+        
+        
+    obj = {
+        'price_data': {
+            'currency': 'usd',
+            'product_data': {
+                'name': product.name,
+            },
+            'unit_amount': product.price,
+        },
+        'quantity': item['quantity']
+    }
+    
+    items.append(obj)
+    
+    stripe.api_key = settings.STRIPE_API_KEY_HIDDEN
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=items,
+        mode='payment',
+        success_url='http://127.0.0.1:8001/cart/success/',
+        cancel_url='http://127.0.0.1:8001/cart',
+    )
+    payment_intent = session.payment_intent
+
+
+    first_name = request.data['first_name']
+    last_name = request.data['last_name']
+    email = request.data['email']
+    address = request.data['address']
+    zipcode = request.data['zipcode']
+    place = request.data['place']
+    phone = request.data['phone']
+    
+    order = Order.objects.create(user=request.user, first_name=first_name, last_name=last_name, email=email, phone=phone, address=address, zipcode=zipcode, place=place)
+
+    order.payment_intent = payment_intent
+    order.paid_amount = total_price
+    order.paid = True
+    order.save()
+        
+    for item in cart:
+        product = item['product']
+        quantity = int(item['quantity'])
+        price = product.price * quantity
+        
+        item = OrderItem.objects.create(order=order, product=product, price=price, quantity=quantity)
+            
+    return JsonResponse({'session': session, 'order': payment_intent})
