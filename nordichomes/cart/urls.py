2f4b5e13de5029bca37b57687a4c3b96e8a7221a
julius <nyerereoffice@gmail.com>
2024-04-17 19:50:07 +0300

    Clear Cart

--- nordichomes/cart/urls.py
+++ nordichomes/cart/urls.py
@@ -7,6 +7,7 @@ app_name = 'cart'
 
 urlpatterns = [
     path('', views.cart, name='cart'),
+    path('success/', views.success, name='success'),
     path('checkout/', views.checkout, name='checkout'),
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('update_cart/<int:product_id>/<str:action>/', views.update_cart, name='update_cart'),
