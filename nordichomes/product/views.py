0bbccd91e6a262d38cd9caded1fb753411aadd36
julius <nyerereoffice@gmail.com>
2024-04-19 12:35:30 +0300

    Create Form for Reviews

--- nordichomes/product/views.py
+++ nordichomes/product/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 
-from .models import Product
+from .models import Product, Review
 
 
 def product(request, slug):
     product = get_object_or_404(Product, slug=slug)
+    
+    if request.method == 'POST':
+        rating = request.POST.get('rating', 3)
+        content = request.POST.get('content', '')
+        
+        if content:
+            review = Review.objects.create(
+                product=product,
+                rating=rating,
+                content=content,
+                created_by=request.user
+            )
+            return redirect("product:product", slug=slug)
     
     return render(request, 'product/product.html', {'product':product})
